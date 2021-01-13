15fe0d075d7c8a30c2c3865e97795ceeee17989b
Guilherme <guilhermebranco800@gmail.com>
2021-01-13 18:27:43 -0300

    Add Radios

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 window.onload = adicionaOptions;
 
-
 function adicionaOptions() {
     var select = document.getElementById("selectOption");
 
@@ -9,40 +8,16 @@ function adicionaOptions() {
         options.className = 'i';
         options.text = i;
         select.appendChild(options);
-
     }
 }
-
-function formatarMoeda() {
-    var valor = document.getElementsByName()
-
-    for (var i = 0; i < valor; i++) {
-        var elemento = document.getElementById('valor');
-        var valor = elemento.value;
-
-        valor = valor + '';
-        valor = parseInt(valor.replace(/[\D]+/g, ''));
-        valor = valor + '';
-        valor = valor.replace(/([0-9]{2})$/g, ",$1");
-
-        if (valor.length > 6) {
-            valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
-        }
-
-        elemento.value = valor;
-
-    }
-
-
-}
-
 
 function adicionaCampos() {
+    
     var select = document.getElementById("selectOption");
     var index = select.selectedIndex;
     var divAtual = document.getElementById("inputs");
 
-    var elementos = document.getElementsByName("inputOption");
+    var elementos = document.getElementsByTagName("input");
 
     if (elementos.length > 0) {
         for (var i = elementos.length - 1; i >= 0; --i) {
@@ -51,15 +26,70 @@ function adicionaCampos() {
     }
 
     for (var i = 0; i < index; i++) {
+        
+        var inputDiderenca = document.createElement("input");
         var input = document.createElement('input');
-        input.name = "inputOption";
-        input.id = i;
-        input.className = "form-control mt-3";
+        var inputRadioSobra = document.createElement('input');
+        var inputRadioFalta = document.createElement('input');
+        var inputRadioZerado = document.createElement('input');
+
+        inputDiderenca.id = i;
+        inputDiderenca.name = "diferenca" +i;
+        inputDiderenca.className = "diferenca";
+        inputDiderenca.placeholder = "Insira a difernça";
+        inputDiderenca.type = "hidden";
+
+        inputRadioSobra.name  = "inputRadio" +i;
+        inputRadioFalta.name  = "inputRadio" +i;
+        inputRadioZerado.name = "inputRadio" +i;
+
+        inputRadioSobra.id    = "Sobra" +i;
+        inputRadioFalta.id    = "Falta" +i;
+        inputRadioZerado.id   = "Zerado"+i;
+
+        inputRadioSobra.value    = "Sobra";
+        inputRadioFalta.value    = "Falta";
+        inputRadioZerado.value   = "Zerado";
+
+        inputRadioSobra.className    = "inputRadio";
+        inputRadioFalta.className    = "inputRadio";
+        inputRadioZerado.className   = "inputRadio";
+
+        inputRadioSobra.type    = "radio";
+        inputRadioFalta.type    = "radio";
+        inputRadioZerado.type   = "radio";
+
+        inputRadioSobra.addEventListener('click',validaRadio);
+        inputRadioFalta.addEventListener('click',validaRadio);
+        inputRadioZerado.addEventListener('click',validaRadio);
+
+        input.name        = "inputOption";
+        input.id          = i;
+        input.className   = "form-control mt-3";
         input.placeholder = i;
-        input.onkeyup = "formatarMoeda();";
+        
         divAtual.appendChild(input);
+
+        divAtual.appendChild(inputRadioSobra);
+        divAtual.appendChild(inputRadioFalta);
+        divAtual.appendChild(inputRadioZerado);
+        divAtual.appendChild(inputDiderenca);
     }
 
+}
+
+function validaRadio()
+{
+    var inputRadio = document.getElementsByClassName("inputRadio");
+    var inputHidden = document.getElementsByClassName("diferenca");
+
+    for(var i =0; i < inputRadio.length; i++)
+    {
+        if(inputRadio[i].checked)
+        {
+            console.log(inputRadio[i].name);
+        }
+    }
 }
 
 
